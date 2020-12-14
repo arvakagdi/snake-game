f985cf6bb253edfb7d74290356ab61570944f897
Arva Kagdi <arvamkagdi@gmail.com>
2020-12-14 13:01:55 -0500

    Change the snake's direction and crete food
    snake.py - we added up, down, left, right methods to change snake's
    direction and conditions to not let snake go from up to down, right to
    left and vice verca
    
    food.py - This file has all the functionalities of food. It inherits
    from Turtle class and it has a color and shape of food and creates food
    randomly inside the scrren
    
    main.py - created a food object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from turtle import Turtle, Screen
 from snake import Snake 
+from food import Food
 import time  # to add delays
 
 
@@ -10,6 +11,14 @@ screen.title("My Snake Game")
 screen.tracer(0)    # stops tracing anything and won't display anthing on screen until we have screen update
  
 snake = Snake()
+food = Food()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
 
 game_is_on = True
 # to have snake's proper movement when it turns, we will start updating from tail (3rd goes to 2nd --> 1 and 1 will change direction if needed)
@@ -18,4 +27,5 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
+    # Detech collision with food
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -2,11 +2,16 @@ from turtle import Turtle
 
 STARTING_POSITIONS  = [(0,0), (-20,0), (-40,0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+RIGHT = 0
+LEFT = 180
 
 class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]  # place this after create_snake else segments would still be empty
 
     def create_snake(self):
         for position in STARTING_POSITIONS:  # setting up intial 3 blocks of snake
@@ -21,7 +26,20 @@ class Snake:
             new_x = self.segments[num - 1].xcor()    # get x coord of prev elem
             new_y = self.segments[num - 1].ycor()    # get y coord of prev elem
             self.segments[num].goto(new_x,new_y)
-        self.segments[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
 
+    def up(self):
+        if self.head.heading()!= DOWN:    # head.heading() returns heading value of head
+            self.head.setheading(90)
 
-
+    def down(self):
+        if self.head.heading()!= UP:     # we can't go from up to down and same for all directions
+            self.head.setheading(270)
+        
+    def left(self):
+        if self.head.heading()!= RIGHT:
+            self.head.setheading(180)
+        
+    def right(self):
+        if self.head.heading()!= LEFT:
+            self.head.setheading(0)
